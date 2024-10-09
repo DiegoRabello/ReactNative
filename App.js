80691c9bfb0a6ca6980b3b5a742218d87e2a288a
DiegoRabello <rabellodiego4677@gmail.com>
2024-10-09 19:47:01 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  text: {
+    color: 'black',
+    fontSize: 32,
+    fontFamily: 'Inter',
+    fontWeight: 'bold',
+  }
+});
